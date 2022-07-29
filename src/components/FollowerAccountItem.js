87928dcd94d3e332d7a9d48855563913ec7493be
aparna316 <aparna.sr012@gmail.com>
2022-07-29 15:10:47 +0530

    ap

--- src/components/FollowerAccountItem.js
+++ src/components/FollowerAccountItem.js
@@ -54,12 +54,12 @@ import { RiCheckFill, RiUserFollowFill } from "react-icons/ri";
         <Hashicon value={props.id} size={50} />
       </div>
       <div className="mx-3 fw-bold">
-        <Link to="/newsfeed/profile" className="text-decoration-none  " style={{color: "#e9d1e0"}}onClick={handleClick} >
+        <Link to="/newsfeed/profile" className="text-decoration-none  " style={{color: "white"}}onClick={handleClick} >
           {props.firstName + " " + props.lastName}
         </Link>
       </div>
       <div>
-        <Button style={{backgroundColor: "#ff0000"}} variant={tickIconStatus ? "primary" : "success"} onClick={handleFollowButtonClick} disabled={tickIconStatus} >
+        <Button style={{backgroundColor: "black"}} variant={tickIconStatus ? "primary" : "success"} onClick={handleFollowButtonClick} disabled={tickIconStatus} >
           {followButtonTitle}{" "}
           {tickIconStatus ? <RiCheckFill /> : <RiUserFollowFill />}
         </Button>
